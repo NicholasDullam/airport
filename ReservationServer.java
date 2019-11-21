44dea260b05e609a853e18f4f5e4ca808c9ce3ea
NichDullam <npdullam@gmail.com>
2019-11-21 12:20:26 -0500

    ReservationServer query implementation start

--- ReservationServer.java
+++ ReservationServer.java
@@ -26,15 +26,15 @@ public class ReservationServer {
         }
     }
 
-    public int getCapacityLeft(Airline airline) {
-        return airline.getCapacityLeft();
+    public static int getCapacityLeft(String name) {
+        return getAirline(name).getCapacityLeft();
     }
 
-    public Passenger[] getPassengers(Airline airline) {
-        return airline.getPassengers();
+    public static Passenger[] getPassengers(String name) {
+        return getAirline(name).getPassengers();
     }
 
-    public Airline getAirline(String name) {
+    public static Airline getAirline(String name) {
         for (Airline airline : airlines) {
             if (airline.getName().toUpperCase().equals(name)) {
                 return airline;
@@ -52,7 +52,26 @@ public class ReservationServer {
         }
 
         public void run() {
-            System.out.println("running");
+            while (true) {
+                BufferedWriter bfw = null;
+                BufferedReader bfr = null;
+
+                try {
+                    bfw = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+                    bfr = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                    String[] query = bfr.readLine().split("$");
+
+                    switch (query[1]) {
+                        case "CAP" :
+                            bfw.write(ReservationServer.getCapacityLeft(query[0]));
+                            break;
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
