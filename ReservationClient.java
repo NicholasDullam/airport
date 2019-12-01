bba9a02e73a6f349b916d5a05b31280a237eeb1a
Michael Taylor <54644976+AAempty@users.noreply.github.com>
2019-11-30 19:07:04 -0600

    Update ReservationClient.java

--- ReservationClient.java
+++ ReservationClient.java
@@ -8,37 +8,37 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
+import static java.lang.Integer.parseInt;
+
 public class ReservationClient extends JFrame {
+
 
     public static void main(String[] args) {
 
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
+
+                Passenger x = new Passenger();
                 String hostName = "";
                 String portNum = "";
-                String thisAirline = "";
 
-
-                /*while (hostName.equals("")) {
+                while (hostName.equals("")) {
                     hostName = inputHostName();
                     if (hostName.equals(JOptionPane.CANCEL_OPTION)) {
                         System.exit(0);
                     }
                 }
-                System.out.println(hostName);
                 while (portNum.equals("")) {
                     portNum = inputPortNum();
                     if (portNum.equals(JOptionPane.CANCEL_OPTION)) {
                         System.exit(0);
                     }
-                }*/
-                //welcome();
-                flightChoice();
-
+                }
+                welcome(x);
+                //*/
             }
         });
-
     }
 
     private static String inputHostName() {
@@ -53,7 +53,8 @@ public class ReservationClient extends JFrame {
                 JOptionPane.INFORMATION_MESSAGE);
     }
 
-    private static void welcome() {
+    private static void welcome(Passenger x) {
+        // TODO: 11/30/2019 add image
         JFrame frame = new JFrame("Purdue University Flight Reservation System");
         JPanel panel = new JPanel();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -61,13 +62,12 @@ public class ReservationClient extends JFrame {
         frame.setSize(600, 400);
         frame.setVisible(true);
 
-
         Font font1 = new Font("SansSerif", Font.BOLD, 20);
 
         JButton exitButton = new JButton("Exit");
         JButton bookFlightButton = new JButton("Book a Flight");
         bookFlightButton.addActionListener(e -> {
-            bookFlight();
+            bookFlight(x);
             frame.setVisible(false);
         });
         exitButton.addActionListener(e -> {
@@ -85,11 +85,10 @@ public class ReservationClient extends JFrame {
         JPanel welcomeText = new JPanel();
         welcomeText.add(text1);
         frame.add(text1, BorderLayout.NORTH);
-
-        //frame.setLocationRelativeTo(null);
     }
 
-    public static void bookFlight() {
+
+    public static void bookFlight(Passenger x) {
         JFrame frame = new JFrame("Purdue University Flight Reservation System");
         JPanel panel = new JPanel();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -105,7 +104,8 @@ public class ReservationClient extends JFrame {
 
         JButton bookFlightButton = new JButton("Yes, I want to book a Flight.");
         bookFlightButton.addActionListener(e -> {
-            flightChoice();
+            frame.setVisible(false);
+            flightChoice(x);
         });
 
         String text = "Do you want to book a flight today?";
@@ -121,7 +121,7 @@ public class ReservationClient extends JFrame {
         frame.add(panel, BorderLayout.SOUTH);
     }
 
-    public static void flightChoice() {
+    public static void flightChoice(Passenger x) {
         JFrame frame = new JFrame("Purdue University Flight Reservation System");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600, 400);
@@ -143,7 +143,8 @@ public class ReservationClient extends JFrame {
         listenerButton.addKeyListener(new KeyAdapter() {
             public void keyPressed(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_BACK_SLASH) {
-                    System.out.println("Enter has been pressed!");
+                    // TODO: 11/30/2019 display flight manifest
+                    //System.out.println("Enter has been pressed!");
                 }
             }
         });
@@ -152,15 +153,7 @@ public class ReservationClient extends JFrame {
         comboBox.addItem("Alaska");
         comboBox.addItem("Delta");
         comboBox.addItem("Southwest");
-        comboBox.addItemListener(listener -> {
-            String choice;
-            JComboBox getSelection = (JComboBox) listener.getSource();
-            choice = (String) getSelection.getSelectedItem();
-            //System.out.println(choice);
-            airlineChoice.replaceAll("", choice);
-            System.out.println(airlineChoice);
 
-        });
         JButton exitButton = new JButton("Exit");
         exitButton.addActionListener(e -> {
             System.exit(0);
@@ -169,8 +162,20 @@ public class ReservationClient extends JFrame {
         JButton chooseButton = new JButton("Choose this flight");
         chooseButton.addActionListener(e -> {
             int choice = comboBox.getSelectedIndex();
+            switch (choice) {
+                case 0:
+                    x.setBoardingPass(new BoardingPass(new Alaska()));
+                    break;
+                case 1:
+                    x.setBoardingPass(new BoardingPass(new Delta()));
+                    break;
+                case 2:
+                    x.setBoardingPass(new BoardingPass(new Southwest()));
+                    break;
+            }
+
             frame.setVisible(false);
-            confirmAirline(airlines[choice]);
+            confirmAirline(x);
         });
 
         String alaskanText = "<html>Alaskan Airlines is proud to serve the strong and knowledgeable Boilermakers " +
@@ -201,8 +206,6 @@ public class ReservationClient extends JFrame {
                             "for much cheaper than other airlines, and offer two free checked bags.<br>We hope you choose " +
                             "Southwest for your next flight.</html>");
             }
-
-
         });
 
         airlineText.add(textBox);
@@ -216,14 +219,15 @@ public class ReservationClient extends JFrame {
 
     }
 
-    public static void confirmAirline(String airline) {
+    public static void confirmAirline(Passenger x) {
         JFrame frame = new JFrame("Purdue University Flight Reservation System");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600, 400);
         frame.setVisible(true);
 
         Font font1 = new Font("SansSerif", Font.BOLD, 20);
-        String text = "<html>Are you sure that you want to book a flight on <br>" + airline + " Airlines?</html>";
+        String text = "<html>Are you sure that you want to book a flight on <br>" + x.getBoardingPass().getAirlineString()
+                + " Airlines?</html>";
         JLabel text1 = new JLabel(text, SwingConstants.CENTER);
         text1.setFont(font1);
         frame.add(text1, BorderLayout.NORTH);
@@ -234,12 +238,13 @@ public class ReservationClient extends JFrame {
         });
         JButton yesButton = new JButton("Yes, I want this flight.");
         yesButton.addActionListener(e -> {
-
+            frame.setVisible(false);
+            askInfo(x);
         });
         JButton noButton = new JButton("No, I want a different flight.");
         noButton.addActionListener(e -> {
             frame.setVisible(false);
-            flightChoice();
+            flightChoice(x);
         });
         JPanel panel = new JPanel();
         panel.add(exitButton, BorderLayout.SOUTH);
@@ -248,9 +253,129 @@ public class ReservationClient extends JFrame {
         frame.add(panel, BorderLayout.SOUTH);
     }
 
-    public void askInfo(){
-        
+    public static void askInfo(Passenger x) {
+        JFrame frame = new JFrame("Purdue University Flight Reservation System");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 400);
+        frame.setVisible(true);
+
+        JButton exitButton = new JButton("Exit");
+        exitButton.addActionListener(e -> {
+            System.exit(0);
+        });
+
+        Font font1 = new Font("SansSerif", Font.BOLD, 20);
+        Font font2 = new Font("SansSerif", Font.PLAIN, 15);
+        String text = "Please input your information below.";
+        JLabel text1 = new JLabel(text, SwingConstants.CENTER);
+        text1.setFont(font1);
+        frame.add(text1, BorderLayout.NORTH);
+
+        JLabel text2 = new JLabel("What is your first name?");
+        text2.setFont(font2);
+
+        JLabel text3 = new JLabel("What is your last name?");
+        text3.setFont(font2);
+
+        JLabel text4 = new JLabel("What is your age?");
+        text4.setFont(font2);
+
+        JTextArea textFieldFN = new JTextArea(4, 50);
+        JTextArea textFieldLN = new JTextArea(4, 50);
+        JTextArea textFieldAGE = new JTextArea(4, 50);
+
+        JButton nextButton = new JButton("Next");
+        nextButton.addActionListener(e -> {
+            if (textFieldFN.getText().equals("") || textFieldLN.getText().equals("") || textFieldAGE.getText().equals("")) {
+                JOptionPane.showConfirmDialog(null, "Please fill out all required fields then press \"Next\" to continue", "ERROR", JOptionPane.ERROR_MESSAGE);
+            } else {
+                if (!confirmInfo(textFieldFN.getText(), textFieldLN.getText(), textFieldAGE.getText())) {
+
+                } else {
+                    frame.setVisible(false);
+                    String firstName = textFieldFN.getText();
+                    String lastName = textFieldLN.getText();
+                    String age = textFieldAGE.getText();
+                    x.setFirstName(firstName);
+                    x.setLastName(lastName);
+                    x.setAge(age);
+                    x.getBoardingPass().setPassenger(x);
+                    x.toString();
+                    flightData(x);
+                }
+            }
+        });
+
+        JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
+        panel1.add(text2);
+        panel1.add(textFieldFN);
+        panel1.add(text3);
+        panel1.add(textFieldLN);
+        panel1.add(text4);
+        panel1.add(textFieldAGE);
+        panel2.add(exitButton);
+        panel2.add(nextButton);
+        frame.add(panel1);
+        frame.add(panel2, BorderLayout.SOUTH);
     }
 
+    public static boolean confirmInfo(String firstN, String lastN, String age) {
+        int choice = -1;
+        String message = "<html>Are all the details you entered correct?<br>" +
+                "The passenger's name is " + firstN + " " + lastN + " and their age is " + age + ".<br>" +
+                "If all the information shown is correct, select the Yes<br> button below, otherwise, " +
+                "select the No button.</html>";
+        choice = JOptionPane.showConfirmDialog(null, message, "Confirm Info",
+                JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE);
+        if (choice == JOptionPane.NO_OPTION) {
+            return false;
+        }
+        if (choice == JOptionPane.YES_OPTION) {
+            return true;
+        } else {
+            return true;
+        }
 
-}
+    }
+
+    public static void flightData(Passenger x) {
+        System.out.println(x.toString());
+        JFrame frame = new JFrame("Purdue University Flight Reservation System");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 400);
+        frame.setVisible(true);
+        JPanel panel = new JPanel();
+
+        JButton exitButton = new JButton("Exit");
+        exitButton.addActionListener(e -> {
+            System.exit(0);
+        });
+        JButton refreshButton = new JButton("Refresh Flight Status");
+        refreshButton.addActionListener(e -> {
+            // TODO: 11/30/2019 refresh
+        });
+
+        Font font1 = new Font("SansSerif", Font.BOLD, 20);
+        String text = "<html>Flight data displaying for " + x.getBoardingPass().getAirlineString() + " Airlines<br>" +
+                "Enjoy your flight!<br>Flight is now boarding at Gate A16</html>";
+        JLabel text1 = new JLabel(text, SwingConstants.CENTER);
+        text1.setFont(font1);
+        frame.add(text1, BorderLayout.NORTH);
+
+        Font font2 = new Font("SansSerif", Font.PLAIN, 15);
+        String text3 = "<html>--------------------------------------------------------------------------------------------------------------------------------------<br>" +
+                "BOARDING PASS FOR FLIGHT 18000 WITH " + x.getBoardingPass().getAirlineString() + " Airlines<br>" +
+                "PASSENGER FIRST NAME: " + x.getFirstName() + "<br>" +
+                "PASSENGER LAST NAME: " + x.getLastName() + "<br>" +
+                "PASSENGER AGE: " + x.getAge() + "<br>" +
+                "You can now begin boarding at gate A16<br>" +
+                "--------------------------------------------------------------------------------------------------------------------------------------</html>";
+        JLabel text4 = new JLabel(text3);
+        text1.setFont(font1);
+        panel.add(text4, BorderLayout.NORTH);
+        panel.add(exitButton, BorderLayout.SOUTH);
+        panel.add(refreshButton, BorderLayout.SOUTH);
+        frame.add(panel, BorderLayout.SOUTH);
+
+    }
