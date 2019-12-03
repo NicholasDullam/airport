f0a995a78b1c7ae5f747e091081f51da10958b99
Michael Taylor <54644976+AAempty@users.noreply.github.com>
2019-12-03 00:01:58 -0600

    Update Southwest.java

--- Southwest.java
+++ Southwest.java
@@ -1,10 +1,9 @@
 /**
- *  Project 5 - Airport Manager
+ * Project 5 - Airport Manager
  *
- *  @author Nicholas Dullam ndullam
- *
- *  @version 11-20-19
- *
+ * @author Nicholas Dullam ndullam
+ * @author Michael Taylor taylo874
+ * @version 11-20-19
  */
 
 public class Southwest extends Airline {
@@ -17,7 +16,8 @@ public class Southwest extends Airline {
     }
 
     public String getDescription() {
-        return "<html><div style='text-align: center;'>Southwest Airlines is proud to offer flights to Purdue University.<br> We are happy " +
+        return "<html><div style='text-align: center;'>Southwest Airlines is proud to offer flights to Purdue " +
+                "University.<br> We are happy " +
                 "to offer free in flight WiFi, as well as our amazing snacks.<br> In addition, we offer flights" +
                 "for much cheaper than other airlines, and offer two free checked bags.<br>We hope you choose " +
                 "Southwest for your next flight.</div></html>";
